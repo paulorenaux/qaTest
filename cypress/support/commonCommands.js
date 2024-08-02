2cd9b97821a48010033010c90ea17b74da8292f3
linux-paulo <paulo_renaux@hotmail.com>
2024-08-01 21:22:07 -0300

    applying corrections by prettier

--- cypress/support/commonCommands.js
+++ cypress/support/commonCommands.js
@@ -4,15 +4,14 @@
 // project.
 // ***********************************************
 
-export function advanceDate (currentDate, addedDays) {
-  let year = currentDate.getFullYear()
-  let month = currentDate.getMonth()
+export function advanceDate(currentDate, addedDays) {
+  let year = currentDate.getFullYear();
+  let month = currentDate.getMonth();
 
   /** identify if it is a leap year, used to identify maximum days in February */
-  const leapYear =
-    (year % 100 != 0 || year % 400 == 0) && year % 4 == 0;
-  
-  const maxDays = 
+  const leapYear = (year % 100 != 0 || year % 400 == 0) && year % 4 == 0;
+
+  const maxDays =
     month == 1 && leapYear
       ? 29
       : month == 1 && !leapYear
@@ -20,22 +19,22 @@ export function advanceDate (currentDate, addedDays) {
       : [0, 2, 4, 6, 7, 9, 11].includes(month)
       ? 31
       : 30;
-      
+
   // if not skipping month
   if (maxDays > addedDays) {
-    return new Date(year, currentDate.getMonth(), currentDate.getDate() + addedDays)
+    return new Date(
+      year,
+      currentDate.getMonth(),
+      currentDate.getDate() + addedDays
+    );
   }
-  
+
   // if should skip month after december
   if (month == 11) {
-    year = year + 1
-    month = 0
+    year = year + 1;
+    month = 0;
   }
-  
-  // days to add after skipping month
-  return advanceDate(new Date(year, month + 1, 0), addedDays - maxDays)
-}
 
-export function getTimestamp (date) {
-  return (new Date(date.getFullYear(), date.getMonth(), date.getDate(), 0, 0, 0, 0)).getTime();
+  // days to add after skipping month
+  return advanceDate(new Date(year, month + 1, 0), addedDays - maxDays);
 }
