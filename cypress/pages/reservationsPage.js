efb36918afafc251257dd81942d3a8410b07ec1a
linux-paulo <paulo_renaux@hotmail.com>
2024-08-01 21:20:44 -0300

    improved functionality to select seat, added functionality to confirm reservation, added functionality to fill passenger information

--- cypress/pages/reservationsPage.js
+++ cypress/pages/reservationsPage.js
@@ -1,7 +1,12 @@
-import jQuery from "jquery";
 
 const body = '[class="reservations show sales"]';
 const seatAvailable = '[class="seat available"]';
+const nextButton = '[id="next-button"]';
+const passengerNameInput =
+  '[id="order_reservations_attributes_0_seats_attributes_0_name"]';
+const passengerDocumentInput =
+  '[id="order_reservations_attributes_0_seats_attributes_0_rg"]';
+const passengerBirthDateInput = '[id="birthdate"]';
 
 export const selectAnySeat = () => {
   cy.get(body)
@@ -11,6 +16,28 @@ export const selectAnySeat = () => {
       return Math.floor(Math.random() * availableSeatCount);
     })
     .then((selectedSeatIndex) => {
-      cy.get(seatAvailable).eq(selectedSeatIndex).click();
+      // using `force: true` due to having parent element () with proprty `display: none`
+      cy.get(seatAvailable)
+        .eq(selectedSeatIndex)
+        .scrollIntoView()
+        .click({ force: true });
     });
+};
+
+export const confirmReservation = () => {
+  cy.get(nextButton).scrollIntoView().click();
+};
+
+export const fillPassengerInfo = (fullName, document, birthdate) => {
+  cy.get(passengerNameInput).type(fullName);
+  cy.get(passengerDocumentInput).type(document);
+  const month =
+    birthdate.getMonth() < 10
+      ? `0${birthdate.getMonth()}`
+      : birthdate.getMonth();
+  const day =
+    birthdate.getDate() < 10 ? `0${birthdate.getDate()}` : birthdate.getDate();
+  cy.get(passengerBirthDateInput).type(
+    `${birthdate.getFullYear()}-${month}-${day}`
+  );
 };
