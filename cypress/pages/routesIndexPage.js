3a8d30fc7f0390ec9179154b6a6b585d494bf653
linux-paulo <paulo_renaux@hotmail.com>
2024-08-01 21:21:19 -0300

    added functionality to select route in routes page

--- cypress/pages/routesIndexPage.js
+++ cypress/pages/routesIndexPage.js
@@ -1,11 +1,10 @@
-
 const body = '[class="routes index sales"]';
-const routesFound = '[id="routes-found"]';
-const routeRow = '[class="route row"]';
-const newReservation = '[class="new_reservation"]';
+/** form containing the button to select seat. */
+const routeForm = '[id*="route-form"]';
 
 export const selectRoute = () => {
   cy.get(body).should("be.visible");
   // As specified by challenge, will select the first returned route.
-  cy.get('[id*="route-form"]').first().click();
-}
+  // Add timeout to wait for response to return and the elements be rendered.
+  cy.get(routeForm, { timeout: 3000 }).first().click();
+};
