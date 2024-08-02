d3f34496134ddb0f5d1bd00c31f5544379cbaf50
linux-paulo <paulo_renaux@hotmail.com>
2024-08-01 21:16:58 -0300

    added login step and fill passenger information step

--- cypress/e2e/homeBuyTicket.cy.js
+++ cypress/e2e/homeBuyTicket.cy.js
@@ -1,26 +1,35 @@
 import { login, searchRoute } from "../pages/homePage";
 import { selectRoute } from "../pages/routesIndexPage";
-import { selectAnySeat } from '../pages/reservationsPage';
+import {
+  fillPassengerInfo,
+  selectAnySeat,
+  confirmReservation,
+} from "../pages/reservationsPage";
 import { advanceDate } from "../support/commonCommands";
 
+// for creating a random brazilian name and document
+const fakerBr = require("faker-br");
+
 describe("home page", () => {
-  const departingCity = "Curitiba, PR";
-  const arrivingCity = "Ponta Grossa, PR";
-  const departingDate = advanceDate(new Date(), 40);
+  const departingCity = "Curitiba - PR";
+  const arrivingCity = "Ponta Grossa - PR";
+  const departingDate = advanceDate(new Date(), 15);
+  const passengerName = fakerBr.name.findName();
+  const passengerDocument = fakerBr.definitions.br.rg();
+  const passengerBirthDate = new Date();
 
-  xit("buy ticket flow", () => {
+  it("buy ticket flow", () => {
+    cy.viewport(1000, 900);
     cy.fixture("staging").then((fixture) => {
-      cy.visit(fixture.baseUrl);
       login(fixture.baseUrl, fixture.email, fixture.password);
-    });
-  });
 
-  it("test without logging in", () => {
-    cy.fixture("staging").then((fixture) => {
-      cy.visit(fixture.baseUrl);
+      // return to home page without using visit (cy.visit will lose the logged in session)
+      cy.get("header").find('[href="/"]').eq(0).click();
     });
     searchRoute(departingCity, arrivingCity, departingDate);
     selectRoute();
     selectAnySeat();
+    confirmReservation();
+    fillPassengerInfo(passengerName, passengerDocument, passengerBirthDate);
   });
 });
